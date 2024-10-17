1df8c6c3cb9990647574aa75965f773d6216d3d1
sVampified <dmblmf2008@gmail.com>
2024-10-16 18:57:07 -0700

    commit of the day, decent progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/vision/VisionPipelineColorDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/vision/VisionPipelineColorDetection.java
@@ -7,50 +7,62 @@ import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class VisionPipelineColorDetection extends OpenCvPipeline {
-
-    Selected selection = Selected.NONE;
     Mat outputFrame = new Mat();
-
-    public Selected getCurrentColorSelection() {
-        return selection;
-    }
+    Mat changedFrame = new Mat();
 
     @Override
     public Mat processFrame(Mat frame) {
-        Imgproc.cvtColor(frame, outputFrame, Imgproc.COLOR_RGB2HSV);
+        Mat modFrame = modifyFrame(frame);
+        double frameHue = getAvgHue(modFrame);
 
-        double frameHue = getAvgHue(outputFrame);
+        return determineColorSelection(frameHue);
+    }
 
+    public Selected determineColorSelection(double frameHue) {
 
         // Variables are all Hue values of HSV
-        double redThresholdHValueStart = 355; // hsv(360, 92%, 60%)
+        double redThresholdHValueStart = 355;
         double redThresholdHValueEnd = 360;
 
         double redThresholdHValueStartSecond = 2;
         double redThresholdHValueEndSecond = 8;
 
-        double blueThresholdHValueStart = 228;    //hsv(220, 92%, 23%)
+        double blueThresholdHValueStart = 228;
         double blueThresholdHValueEnd = 233;
 
-        double yellowThresholdHValueStart = 39; // hsv(44, 88%, 100%)
+        double yellowThresholdHValueStart = 39;
         double yellowThresholdHValueEnd = 43;
 
         if ((frameHue >= redThresholdHValueStart && frameHue <= redThresholdHValueEnd) ||
                 (frameHue >= redThresholdHValueStartSecond && frameHue <= redThresholdHValueEndSecond)) {
-            selection = Selected.RED;
+            return Selected.RED;
         } else if (frameHue >= blueThresholdHValueStart && frameHue <= blueThresholdHValueEnd) {
-            selection = Selected.BLUE;
+            return Selected.BLUE;
         } else if (frameHue >= yellowThresholdHValueStart && frameHue <= yellowThresholdHValueEnd) {
-            selection = Selected.NEUTRAL;
+            return Selected.NEUTRAL;
         }
-            // Compares saturation to others to determine color of object
-            // return the selected color currently return Selected.BLUE;
-        return selection;
+
+        return Selected.NONE;
     }
 
     public double getAvgHue(Mat selectedFrame) {
         Scalar color = Core.mean(selectedFrame);
         return color.val[0] * (360.0 / 179.0);
+    }
+
+    public Mat modifyFrame(Mat frame) {
+        // Region of interest to extract a certain part of the camera frame
+        int roiStartX = frame.cols() / 4;
+        int roiStartY = frame.rows() / 4;
+        int roiWidth = frame.cols() / 2;
+        int roiHeight = frame.rows() / 2;
+
+        changedFrame = frame.submat(roiStartY, roiStartY + roiHeight, roiStartX, roiStartX + roiWidth);
+
+        Imgproc.cvtColor(changedFrame, changedFrame, Imgproc.COLOR_RGB2HSV);
+
+
+        return changedFrame;
     }
 
     public enum Selected {
