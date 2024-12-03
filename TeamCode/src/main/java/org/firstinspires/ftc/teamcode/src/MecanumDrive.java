72e624fd2e2d00a064483243eb83f8af3b0d2c8f
Alexwen709 <alexwen709@gmail.com>
2024-12-02 16:33:26 -0800

    update commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/MecanumDrive.java
@@ -32,6 +32,7 @@ package org.firstinspires.ftc.teamcode.src;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -78,9 +79,9 @@ public class MecanumDrive {
         this.backLeftDrive = hardwareMap.get(DcMotor.class, "bl_drive");
         this.backRightDrive = hardwareMap.get(DcMotor.class, "br_drive");
 
-        this.frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        this.frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         this.frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
-        this.backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        this.backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         this.backRightDrive.setDirection(DcMotor.Direction.FORWARD);
 
         this.frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
